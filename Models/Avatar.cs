f494aa6d29b8110a116de58d3c3679cc02d9a868
DESKTOP-1DUM6AO\aaron <aaronballes11@svalero.com>
2024-06-09 22:33:45 +0200

    Fin.

--- Models/Avatar.cs
+++ Models/Avatar.cs
@@ -13,7 +13,7 @@ namespace GalacticApi.Models
         public string CodigoJuego {get; set; }//este de aquí tiene que ver con el codigo del juego donde se consigue
 
         public List<Usuario> Usuarios{ get; set; }
-        //no se va a usar
+        //no se va a usar2
 
         public Avatar(){
 
